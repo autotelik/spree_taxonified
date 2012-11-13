eefb9c81f4f67dc6bd39aa98dd8b36718a77e460
Thomas Statter <tom@autotelik.co.uk>
2012-11-13 10:16:28 +0000

    basic structures in place

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -8,16 +8,7 @@ module Spree
     accepts_nested_attributes_for :products_taxons, :allow_destroy => true
 
 
-    # Deleting root taxon is deadly  - delete through Taxonomy
-    before_destroy :ensure_not_root
-  
-    def ensure_not_root
-      return false if self.taxonomy && self.parent_id.nil?
-    end
-
-    belongs_to :page
-
-    named_scope :for_menu,  lambda { |*args| { :conditions => ["menu = ?", args.first || 'topNav'], :order => :menu_priority } }
+    named_scope :for_menu,  lambda { |*args| { :conditions => ["menu = ?", args.first || 'top'], :order => :menu_priority } }
 
     def css_name
       self.name.gsub(' ','_')
